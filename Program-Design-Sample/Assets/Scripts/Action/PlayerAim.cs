d29b5dbe8b9574fb513a5ad92eb06b7d77340423
KYapp <kukiyomenaisapp@gmail.com>
2023-06-22 00:18:52 +0900

    chaos

--- Program-Design-Sample/Assets/Scripts/Action/PlayerAim.cs
+++ Program-Design-Sample/Assets/Scripts/Action/PlayerAim.cs
@@ -1,39 +1,35 @@
-using System;
-using Action.Controller;
 using Cinemachine;
 using Extensions.Quaternion;
 using Input.Interface;
 using UniRx;
-using UniRx.Triggers;
-using Unity.Mathematics;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 namespace Action
 {
     public class PlayerAim : MonoBehaviour
     {
-        [SerializeField] private InterfaceProperty<IAimInput> aimInput=new();
+        [SerializeField] private InterfaceProperty<IAimInput> aimInput = new();
         [SerializeField] private CinemachineBrain _cinemachineBrain;
         [SerializeField] private Transform eye;
-        [SerializeField] private float horizontalSpeed=10;
+        [SerializeField] private float horizontalSpeed = 10;
         [SerializeField] private float verticalSpeed = 10;
-        [SerializeField] private float verticalMin,verticalMax;
+        [SerializeField] private float verticalMin, verticalMax;
 
         private Vector2 aimDeltaInput;
+
         private void Start()
         {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
             var horizontalRotation = transform.localRotation;
             var verticalRotation = eye.localRotation;
-            aimInput.Value.OnAim.Subscribe(delta =>
+            aimInput.Value.OnAim.Subscribe(delta => { aimDeltaInput = delta; }).AddTo(this);
+            Observable.EveryUpdate().Subscribe(_ =>
             {
-                aimDeltaInput = delta;
-            }).AddTo(this);
-            Observable.EveryUpdate().Subscribe(_=>
-            {
-                horizontalRotation *= Quaternion.AngleAxis(aimDeltaInput.x * horizontalSpeed*Time.deltaTime, Vector3.up);
-                verticalRotation *= Quaternion.AngleAxis(aimDeltaInput.y * verticalSpeed*Time.deltaTime, Vector3.right);
+                horizontalRotation *=
+                    Quaternion.AngleAxis(aimDeltaInput.x * horizontalSpeed, Vector3.up);
+                verticalRotation *=
+                    Quaternion.AngleAxis(aimDeltaInput.y * verticalSpeed, Vector3.right);
 
                 verticalRotation = QuaternionUtility.ClampRotation(verticalRotation, verticalMin, verticalMax);
                 transform.localRotation = horizontalRotation;
